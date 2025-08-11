74ab567f5b8878adb63ed34c129971cfd59c62eb
Mostafa Sobhy <161953020+MostafaSobhy4@users.noreply.github.com>
2025-08-11 16:40:15 +0300

    commit

--- JS Files/home.js
+++ JS Files/home.js
@@ -105,9 +105,12 @@ else {
     let must = document.createElement("p");
     let go2login = document.createElement("a");
     go2login.textContent = "Press Here To Login First";
-    go2login.href = "../login.html";
+    go2login.href = "";
     orderAdd.appendChild(must);
     orderAdd.appendChild(go2login);
+    go2login.onclick = function(e) {
+        window.location.href = "../login.html";
+    }
 
     addButtonItem.forEach((butt) => {
         butt.onclick = function() {
